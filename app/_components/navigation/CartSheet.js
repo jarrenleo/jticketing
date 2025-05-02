f6a1c79cd7b6440bf23303d08646a95d7e7d6f8a
jarrenleo <jarren.leoo@gmail.com>
2025-05-02 16:58:41 +0800

    changes

--- app/_components/navigation/CartSheet.js
+++ app/_components/navigation/CartSheet.js
@@ -139,6 +139,7 @@ export default function CartSheet() {
                         src={retrieveImageUrl("events", item.image_file)}
                         alt={item.artist}
                         fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                         className="object-cover"
                       />
                     </div>
