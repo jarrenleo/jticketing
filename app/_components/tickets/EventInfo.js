994b6ae6a1e178073ea69770a3f84afef12f461e
jarrenleo <jarren.leoo@gmail.com>
2025-05-18 20:23:45 +0800

    changes

--- app/_components/tickets/EventInfo.js
+++ app/_components/tickets/EventInfo.js
@@ -1,12 +1,9 @@
-import { Info } from "lucide-react";
-
 export default function EventInfo({ additionalInfo }) {
   if (!additionalInfo) return null;
 
   return (
-    <div className="mb-8 flex items-center gap-2 rounded-md bg-accent p-4 text-accent-foreground">
-      <Info height={16} width={16} />
-      <p className="text-base">{additionalInfo}</p>
-    </div>
+    <span className="mb-8 rounded-md bg-accent p-4 text-accent-foreground">
+      {additionalInfo}
+    </span>
   );
 }
